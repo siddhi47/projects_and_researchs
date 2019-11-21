ddcf6954806204b6692cce17f2a8a142ceaa3114
Siddhi Kiran Bajracharya <siddhikiran.bajracharya@extenso.com>
2019-11-21 12:15:46 +0545

    Give links using json file

--- download_images.py
+++ download_images.py
@@ -2,24 +2,26 @@ import requests
 import time
 import concurrent.futures
 import os
-
-img_urls = [
-    'https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?cs=srgb&dl=nature-red-love-romantic-67636.jpg&fm=jpg'
-]
+import json
 
 
 def download_image(img_url):
-    img_bytes = requests.get(img_url).content
-    img_name = ''.join(e for e in img_url.split('/')[-1] if e.isalnum())
-    img_name = f'{img_name}.jpg'
-    with open('downloads/'+img_name, 'wb') as img_file:
-        img_file.write(img_bytes)
-        print(f'{img_name} was downloaded...')
+    try:
+        img_bytes = requests.get(img_url).content
+        img_name = ''.join(e for e in img_url.split('/')[-1] if e.isalnum())
+        img_name = f'{img_name}.jpg'
+        with open('downloads/'+img_name, 'wb') as img_file:
+            img_file.write(img_bytes)
+    except Exception as e:
+        raise e
 
 
 def main():
     try:
         t1 = time.perf_counter()
+        with open('urls.json') as urls:
+            url_list = json.load(urls)
+            img_urls = url_list["url"]
 
         if not os.path.exists('downloads'):
             os.mkdir('downloads')
